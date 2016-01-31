ae81b1b1974857bf618d4e2b1b1aafaf0a7791fe
Mansimran Singh <me@mansimransingh.com>
2016-01-31 11:27:11 +0000

    a

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -59,7 +59,7 @@
 
                 for (var i=0; i < 10; i ++){
                     if (stats[i]){
-                        stats[i] = (stats*100)/total;
+                        stats[i] = (stats[i]*100)/total;
                     } else {
                         stats[i] = 0;
                     }
