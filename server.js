cd78be262db5fb74d861fb9a6f863bf12dafe2b3
Mansimran Singh <me@mansimransingh.com>
2016-01-31 22:16:03 +0000

    only index

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ app.get('/stats', function(req, res){
 });
 
 // application -------------------------------------------------------------
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
